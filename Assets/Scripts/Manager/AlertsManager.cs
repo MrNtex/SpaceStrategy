2eed55447e8efdf02663743b6cb3dacf166ad525
MrNtex <ntexyt@gmail.com>
2024-06-07 17:08:17 +0200

    fix: Managed to create an optimal solution by creating a global animator

--- Assets/Scripts/Manager/AlertsManager.cs
+++ Assets/Scripts/Manager/AlertsManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 public enum AlertType
 {
     Research,
@@ -14,6 +15,8 @@ public class AlertsManager : MonoBehaviour
 
     [SerializeField]
     private GameObject[] alerts;
+
+    private List<Image> activeAlerts = new List<Image>();
     private void Awake()
     {
         if (Instance == null)
@@ -30,11 +33,15 @@ public class AlertsManager : MonoBehaviour
         alerts[(int)alert].SetActive(true);
 
         alerts[(int)alert].GetComponent<AlertObject>().UpdateTooltipData(td);
+
+        activeAlerts.Add(alerts[(int)alert].GetComponent<Image>());
     }
     public void HideAlert(AlertType alert)
     {
         alerts[(int)alert].SetActive(false);
         alerts[(int)alert].GetComponent<AlertObject>().UpdateTooltipData(null);
+
+        activeAlerts.Remove(alerts[(int)alert].GetComponent<Image>());
     }
     public void OnAlertClick(AlertType alertType)
     {
@@ -48,4 +55,6 @@ public class AlertsManager : MonoBehaviour
                 break;
         }
     }
+
+    
 }
